d1343495ef9f100e6b3c801b600b54f0255bc530
phong-utm <phong@graduate.utm.my>
2022-03-04 08:10:23 +0000

    Use TypeScript

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    transform: {
+      "\\.ts$": "ts-jest",
+    },
+    testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+    setupFilesAfterEnv: [],
+    clearMocks: true,
+  }
+  
